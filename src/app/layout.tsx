157b524bbecc314a81f83f35b695f624169fe700
Youngwoo <youngwoo206@gmail.com>
2023-06-21 00:34:33 -0400

    init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "PulsePoint",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
