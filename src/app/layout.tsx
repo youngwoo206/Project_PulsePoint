8f0a5c85dc58baf9e03c7e07926e10a4d4c57219
Youngwoo <youngwoo206@gmail.com>
2023-06-21 01:01:27 -0400

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+import Navbar from "@/components/ui/Navbar";
 
 export const metadata = {
   title: "PulsePoint",
-  description: "A Reddit clone built with Next.js and TypeScript.",
+  description: "A Reddit clone built with Next.js and TypeScript",
 };
+
+const font = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -11,8 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        font.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
