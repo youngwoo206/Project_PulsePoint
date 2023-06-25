6ccf85da4daec3f811d6e40ce3c54e1e68e2afe9
Youngwoo <youngwoo206@gmail.com>
2023-06-25 14:13:09 -0400

    post voting done

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,14 +1,16 @@
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
+import { redis } from "@/lib/redis"
 import { PostVoteValidator } from "@/lib/validators/vote"
 import { CachedPost } from "@/types/redis"
+import {z} from 'zod'
 
 //controls when caching begins
 const CACHE_AFTER_UPVOTES = 1
 
 export async function PATCH(req: Request) {
     try {
-        const body = req.json()
+        const body = await req.json()
         const { postId, voteType } = PostVoteValidator.parse(body)
         const session = await getAuthSession()
 
@@ -81,13 +83,48 @@ export async function PATCH(req: Request) {
                     currentVote: voteType,
                     createdAt: post.createdAt
                 }
+            
+                await redis.hset(`post:${postId}`, cachePayload)
             }
+
+            return new Response('Vote Registered')
         }
 
+        // If there's no existing vote, creating new vote object
+        await db.vote.create({
+            data: {
+                type: voteType,
+                userId: session.user.id,
+                postId
+            }
+        })
 
+        //recounting votes
+        const votesAmt = post.votes.reduce((acc, vote) => {
+            if (vote.type === "UP") return acc + 1
+            if (vote.type === "DOWN") return acc - 1
+            return acc
+        }, 0)
 
+        if (votesAmt >= CACHE_AFTER_UPVOTES) {
+            const cachePayload: CachedPost = {
+                id: post.id,
+                title: post.title,
+                authorUsername: post.author.username ?? '',
+                content: JSON.stringify(post.content),
+                currentVote: voteType,
+                createdAt: post.createdAt
+            }
+            await redis.hset(`post:${postId}`, cachePayload)
+        }
+        return new Response('Vote Registered')
 
-    } catch (error) {
-        
+    } 
+        catch (error) {
+            if (error instanceof z.ZodError) {
+                return new Response('Invalid request data', {status: 422})
+            }
+
+            return new Response('Unable to register vote', {status: 500})
     }
 }
--- src/components/ui/posts/voting/PostVoteClient.tsx
+++ src/components/ui/posts/voting/PostVoteClient.tsx
@@ -9,7 +9,8 @@ import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import { PostVoteRequest } from "@/lib/validators/vote";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
 
 interface PostVoteClientProps {
   postId: string;
@@ -32,7 +33,7 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
     setCurrentVote(initialVote);
   }, [initialVote]);
 
-  const {} = useMutation({
+  const { mutate: vote } = useMutation({
     mutationFn: async (voteType: VoteType) => {
       const payload: PostVoteRequest = {
         postId,
@@ -41,11 +42,56 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
 
       await axios.patch("/api/subreddit/post/vote", payload);
     },
+    onError: (err, voteType) => {
+      if (voteType === "UP") {
+        setVotesAmt((prev) => prev - 1);
+      } else {
+        setVotesAmt((prev) => prev + 1);
+      }
+
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "Something went wrong",
+        description: "Your vote was not registered as this time",
+        variant: "destructive",
+      });
+    },
+
+    onMutate: (type: VoteType) => {
+      if (currentVote === type) {
+        setCurrentVote(undefined);
+
+        if (type === "UP") {
+          setVotesAmt((prev) => prev - 1);
+        } else if (type === "DOWN") {
+          setVotesAmt((prev) => prev + 1);
+        }
+      } else {
+        setCurrentVote(type);
+        if (type === "UP") {
+          setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+        } else if (type === "DOWN") {
+          setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+        }
+      }
+    },
   });
 
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        onClick={() => vote("UP")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
         <ArrowBigUp
           className={cn("h-5 w-5 text-zinc-700", {
             "text-emerald-500 fill-emerald-500": currentVote === "UP",
@@ -57,10 +103,15 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
         {votesAmt}
       </p>
 
-      <Button size="sm" variant="ghost" aria-label="downvote">
+      <Button
+        onClick={() => vote("DOWN")}
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+      >
         <ArrowBigDown
           className={cn("h-5 w-5 text-zinc-700", {
-            "text-red-500 fill-red-500": currentVote === "UP",
+            "text-red-500 fill-red-500": currentVote === "DOWN",
           })}
         />
       </Button>
