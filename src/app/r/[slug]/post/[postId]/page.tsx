079d8065e41141ce748a7f00ff6409d288f23711
Youngwoo <youngwoo206@gmail.com>
2023-06-26 22:46:57 -0400

    data optimization complete

---
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,64 @@
+import PostVoteServer from "@/components/ui/posts/voting/PostVoteServer";
+import PostVoteShell from "@/components/ui/posts/voting/PostVoteShell";
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { CachedPost } from "@/types/redis";
+import { Post, User, Vote } from "@prisma/client";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+interface PageProps {
+  params: {
+    postId: string;
+  };
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const Page = async ({ params }: PageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteShell />}>
+          {/*@ts-expect-error*/}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/ui/posts/voting/PostVoteShell.tsx
@@ -0,0 +1,24 @@
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+import { buttonVariants } from "../../Button";
+
+const PostVoteShell = () => {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+};
+export default PostVoteShell;
