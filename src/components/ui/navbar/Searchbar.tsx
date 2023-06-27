a10531e424ce01ea5ad0201211708df9c78bb14f
Youngwoo <youngwoo206@gmail.com>
2023-06-27 17:18:18 -0400

    ready for deployment

--- src/components/ui/navbar/Searchbar.tsx
+++ src/components/ui/navbar/Searchbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import {
   Command,
   CommandEmpty,
@@ -11,15 +11,18 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Prisma, Subreddit } from "@prisma/client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Users } from "lucide-react";
 import debounce from "lodash.debounce";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 interface SearchbarProps {}
 
 const Searchbar: FC<SearchbarProps> = ({}) => {
   const [input, setInput] = useState<string>("");
   const router = useRouter();
+  const commandRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
 
   const {
     data: queryResults,
@@ -48,8 +51,19 @@ const Searchbar: FC<SearchbarProps> = ({}) => {
     request();
   }, []);
 
+  useOnClickOutside(commandRef, () => {
+    setInput("");
+  });
+
+  useEffect(() => {
+    setInput("");
+  }, [pathname]);
+
   return (
-    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+    <Command
+      ref={commandRef}
+      className="relative rounded-lg border max-w-lg z-50 overflow-visible"
+    >
       <CommandInput
         isLoading={isFetching}
         value={input}
